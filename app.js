30a56551905acd5203dbf9e0033285ab93ddfcc8
mikaelaliwanag <mikaela301@gmail.com>
2020-07-13 20:49:23 -0700

    Require express, body-parser, and ejs

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set.apply('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
