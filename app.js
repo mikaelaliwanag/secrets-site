ec554402310a70d1e2d782b77a3c1f0f5a36b15b
mikaelaliwanag <mikaela301@gmail.com>
2020-07-15 15:26:29 -0700

    Connect to mongodb

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get("/", function(req, res) {
     res.render("home");
