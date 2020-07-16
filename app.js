c09d83b997d17b8d60f054dedaa144e7c97da85a
mikaelaliwanag <mikaela301@gmail.com>
2020-07-16 16:00:18 -0700

    Update mongoose schema

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.schema ({
     email: String,
     password: String
-};
+});
 
 const User = new mongoose.model("User", userSchema);
 
