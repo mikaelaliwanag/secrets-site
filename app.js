ba2910e1f53dea6e3b3921dc4bd2b60c4fee3b20
mikaelaliwanag <mikaela301@gmail.com>
2020-07-16 17:02:14 -0700

    Create .env file. Update .gitignore

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const encrypt = require("mongoose-encryption");
 
 const app = express();
 
+
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,8 +20,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "Heygetoutofherethisisasecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
