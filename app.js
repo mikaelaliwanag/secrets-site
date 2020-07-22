2d44259551a338c773630350a80eb9a98705a4c8
mikaelaliwanag <mikaela301@gmail.com>
2020-07-21 21:18:35 -0700

    Salt and hash user password

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -34,18 +35,21 @@ app.get("/register", function(req, res) {
 
 //////Register route//////
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save(function(err){
-        if(err) {
+ bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+     newUser.save(function(err){
+         if(err) {
             console.log(err);
         } else {
-            res.render("secrets");
+           res.render("secrets");
         }
     });
+  });
 
 });
 
