1a57efded3e07a49a6f9502a4cd76a27e10ed29b
mikaelaliwanag <mikaela301@gmail.com>
2020-07-23 21:45:25 -0700

    Update register route

--- app.js
+++ app.js
@@ -54,6 +54,16 @@ app.get("/register", function(req, res) {
 //////Register route//////
 app.post("/register", function(req, res){
 
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect.("/secrets");
+            })
+        }
+    })
 
 });
 
