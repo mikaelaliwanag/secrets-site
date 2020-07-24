af6e77b52085be2a056184090b11170aa88e2145
mikaelaliwanag <mikaela301@gmail.com>
2020-07-23 19:07:53 -0700

    Install and require passport and express-session

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -36,43 +37,12 @@ app.get("/register", function(req, res) {
 //////Register route//////
 app.post("/register", function(req, res){
 
- bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-
-     newUser.save(function(err){
-         if(err) {
-            console.log(err);
-        } else {
-           res.render("secrets");
-        }
-    });
-  });
 
 });
 
 //////Login route/////
 app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser) {
-
-        if(err){
-            console.log(err);
-        } else {
-          if(foundUser) {
-              bcrypt.compare(password, foundUser.password, function(err, result) {
-                 if(result === true) {
-                  res.render("secrets");
-                 }
-              });
-            }
-        }
-
-    });
+ 
 
 });
 
