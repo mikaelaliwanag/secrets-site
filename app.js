7ef665e7afe2b711d640873bbe6e4fb902797046
mikaelaliwanag <mikaela301@gmail.com>
2020-07-23 20:01:33 -0700

    Set up session and initialize passport

--- app.js
+++ app.js
@@ -13,14 +13,28 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "Cucumber water is a lie.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+
 
 app.get("/", function(req, res) {
     res.render("home");
