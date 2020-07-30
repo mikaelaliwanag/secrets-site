a082b45f136a2b9f26761ff4cc4cb41fbb7bdc4f
mikaelaliwanag <mikaela301@gmail.com>
2020-07-29 18:24:12 -0700

    Update passport serialization and deserialization logic

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -39,8 +40,15 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
@@ -49,6 +57,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
+
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -67,8 +76,8 @@ app.get("/auth/google",
 app.get("/auth/google/secrets", 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-      //Successful authentication, redirect to secrets:
-    res.redirect('/');
+      //Successful authentication, redirect to secrets.
+    res.redirect('/secrets');
   });
 
 app.get("/login", function(req, res) {
